266f4f00ab973c8de7b09f49e4249d39daafd4b4
JonattanS <jonattanjonattan4@gmail.com>
2022-01-04 22:57:27 +0530

    Pick command fix started

--- commands/info.js
+++ commands/info.js
@@ -1,8 +1,20 @@
 const Discord = require('discord.js'); // For Embedded Message.
-const fs = require('fs');
+const user_model = require('../models/user.js');
 
-module.exports.run = async (bot, message, args) => {
-    
+module.exports.run = async (bot, message, args, prefix, user_available, pokemons) => {
+    if (!user_available) { message.channel.send(`You should have started to use this command! Use ${prefix}start to begin the journey!`); return; }
+
+    //Get user data.
+    user_model.findOne({ UserID: message.author.id }, (err, user) => {
+        if(!user) return;
+        if(err) console.log(err);
+
+        var user_pokemons = user.Pokemons;
+        console.log(user_pokemons);
+        console.log(user.Selected)
+        var selected_pokemon = user_pokemons.filter(it => it._id === user.Selected);
+        console.log(selected_pokemon);
+    });
 }
 
 module.exports.config = {
