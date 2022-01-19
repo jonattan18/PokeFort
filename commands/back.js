f3abd0a2408e7382a6f1f56ae900e4a0fd97abc2
JonattanS <jonattanjonattan4@gmail.com>
2022-01-19 14:21:35 +0530

    Next and Back Commands

--- commands/back.js
+++ commands/back.js
@@ -1,32 +1,29 @@
 const Discord = require('discord.js'); // For Embedded Message.
 
 // Models
-const channel_model = require('../models/channel');
+const pages_model = require('../models/pages');
 
 module.exports.run = async (bot, message, args, prefix, user_available) => {
     if (!user_available) { message.channel.send(`You should have started to use this command! Use ${prefix}start to begin the journey!`); return; }
 
-    channel_model.findOne({ ChannelID: message.channel.id }, (err, channel) => {
+    pages_model.findOne({ ChannelID: message.channel.id, UserID: message.author.id }, (err, pages) => {
         if (err) return console.log(err);
-        if (!channel) return;
+        if (!pages) { message.channel.send(`No messages found to move to previous page!`); return; }
 
-        var pagination = channel.Pagination;
-        var user_page = pagination.filter(x => x.UserID == message.author.id)[0];
-        if (!user_page) { message.channel.send(`No messages found to move to next page!`); return; }
-        var msg = JSON.parse(user_page.Message[0]);
-        var embed = user_page.Embed;
-        var current_page = user_page.CurrentPage;
-        message.delete();
-        if (current_page == 1) return message.channel.send(`You are on the first page!`);
+        var embed = pages.Embed;
+        var current_page = pages.CurrentPage;
+        var total_pages = embed.length;
+
+        message.delete(); // Remove next command sent by user.
+
+        if (current_page == 0) return message.channel.send(`You are on the first page!`);
         else { current_page--; }
-        const new_embed = new Discord.MessageEmbed(embed[current_page - 1]);
-        message.channel.messages.fetch(msg.id)
+        const new_embed = new Discord.MessageEmbed(embed[current_page]);
+        message.channel.messages.fetch(pages.MessageID)
             .then(message_old => {
                 message_old.edit(new_embed);
-                channel_model.findOneAndUpdate({ ChannelID: message.channel.id }, { $set: { "Pagination.$[elem].CurrentPage": current_page } }, { arrayFilters: [{ "elem.UserID": message.author.id }] }, (err, channel) => {
-                    if (err) return console.log(err);
-                    if (!channel) return;
-                });
+                pages.CurrentPage = current_page;
+                pages.save();
             })
             .catch(console.error);
     });
