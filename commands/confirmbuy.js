ca0fc856aaf836f8033233d8d00e317c2844a54f
jonattans <jonattanjonattan4@gmail.com>
2022-06-05 11:39:05 +0530

    Buy

--- commands/confirmbuy.js
+++ commands/confirmbuy.js
@@ -39,26 +39,32 @@ module.exports.run = async (bot, message, args, prefix, user_available, pokemons
                     prompt.remove().then(() => {
                         market.remove().then(() => {
                             user.save().then(() => {
-                           //     bot.users.fetch(market.UserID).then(user => {
-                                    user_model.findOne({ UserID: user.id }, (err, user1) => {
 
-                                        var tax_price = [];
-                                        if (args[2] > 1000 && args[2] <= 10000) tax_price = ["1,000", 1.5, percentCalculation(args[2], 1.5).toFixed(0)];
-                                        else if (args[2] > 10000 && args[2] <= 100000) tax_price = ["10,000", 3, percentCalculation(args[2], 3).toFixed(0)]
-                                        else if (args[2] > 100000 && args[2] <= 1000000) tax_price = ["1,00,000", 4.5, percentCalculation(args[2], 4.5).toFixed(0)];
-                                        else if (args[2] > 1000000) tax_price = ["1,000,000", 6, percentCalculation(args[2], 6).toFixed(0)];
+                                user_model.findOne({ UserID: user.id }, (err, user1) => {
 
-                                        user1.PokeCredits += market.Price - tax_price[2];
-                                        user1.save().then(() => {
-                                            var embed = new Discord.MessageEmbed();
-                                            embed.setTitle(`Congratulations! Your Pokemon was sold!`);
-                                            embed.setDescription(`${tax_price.length > 0 ? ` _As your pokemon sold for over ${tax_price[0]} credits, ${tax_price[1]}% has been taken as tax and you have received ${args[2] - tax_price[2]} credits._\n` : ""}Your ${market.Level} ${market.PokemonName}${market.Shiny == true ? " :star:" : ""} has sold and you have received ${market.Price} Credits.`);
-                                        //    user.send(embed);
-                                        });
+                                    var tax_price = [];
+                                    if (args[2] > 1000 && args[2] <= 10000) tax_price = ["1,000", 1.5, percentCalculation(args[2], 1.5).toFixed(0)];
+                                    else if (args[2] > 10000 && args[2] <= 100000) tax_price = ["10,000", 3, percentCalculation(args[2], 3).toFixed(0)]
+                                    else if (args[2] > 100000 && args[2] <= 1000000) tax_price = ["1,00,000", 4.5, percentCalculation(args[2], 4.5).toFixed(0)];
+                                    else if (args[2] > 1000000) tax_price = ["1,000,000", 6, percentCalculation(args[2], 6).toFixed(0)];
+
+                                    user1.PokeCredits += market.Price - tax_price[2];
+                                    user1.save().then(() => {
+                                        var embed = new Discord.MessageEmbed();
+                                        embed.setTitle(`Congratulations! Your Pokemon was sold!`);
+                                        embed.setDescription(`${tax_price.length > 0 ? ` _As your pokemon sold for over ${tax_price[0]} credits, ${tax_price[1]}% has been taken as tax and you have received ${args[2] - tax_price[2]} credits._\n` : ""}Your ${market.Level} ${market.PokemonName}${market.Shiny == true ? " :star:" : ""} has sold and you have received ${market.Price} Credits.`);
+                                        
+                                        // Send Message
+                                        const owner = bot.users.fetch(market.UserID).catch(() => null);
+                                        if (owner) {
+                                            owner.send(embed).catch(() => { });
+                                        } else bot.users.cache.get(market.UserID).send(embed);
+
                                     });
                                 });
-                                message.channel.send(`Your have bought Level ${market.Level} ${market.PokemonName}${market.Shiny == true ? " :star:" : ""} from market for ${market.Price} Credits.`);
-                          //  });
+                            });
+                            message.channel.send(`Your have bought Level ${market.Level} ${market.PokemonName}${market.Shiny == true ? " :star:" : ""} from market for ${market.Price} Credits.`);
+                            //  });
                         });
                     });
                 });
