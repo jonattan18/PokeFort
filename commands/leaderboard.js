afc0ee8505f9750d43fc32cdfea313d233ed4382
jonattans <jonattanjonattan4@gmail.com>
2022-06-22 10:56:58 +0530

    Lb Fix

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js'); // For message embed.
 const leaderboard_model = require('../models/leaderboard.js');
-
+const user_model = require('../models/user');
 
 module.exports.run = async (bot, message, args, prefix, user_available, pokemons) => {
     if (!user_available) { message.channel.send(`You should have started to use this command! Use ${prefix}start to begin the journey!`); return; }
